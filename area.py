450e35be90f4e33664fe44239fc81211711fc513
Sanskritimalik9 <67856098+Sanskritimalik9@users.noreply.github.com>
2021-02-15 00:01:52 +0530

    python program.py
    This program will give the area of the circle and the second program will give the extension of the file

--- area.py
+++ area.py
@@ -2,3 +2,8 @@ PI=3.14
 radius=float(input("Enter the radius of circle:"))
 area=PI*radius*radius
 print("area of circle = %.2f" %area)
+
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
